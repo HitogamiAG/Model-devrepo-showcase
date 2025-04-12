7a3664d76761ba7e80f450dd07ce8210f9108733
HitogamiAG <gavrilko.al@gmail.com>
2025-04-12 22:14:52 +0500

    add data modules

---
+++ src/settings.py
@@ -0,0 +1,14 @@
+"""Persistent settings for the project"""
+
+CLASS_ENCODING = {
+    "background": [0, 0, 0],
+    "building": [0, 0, 128],
+    "human": [0, 64, 64],
+    "tree": [0, 128, 0],
+    "low_vegetation": [0, 128, 128],
+    "moving_vehicle": [128, 0, 64],
+    "road": [128, 64, 128],
+    "static_vehicle": [192, 0, 192],
+}
+
+IMAGE_CROP_SIZE = 960
